52dd9233ac7799627c97e3965f7af1794ea0eaba
Izaak White <ww655421@ohio.edu>
2025-03-06 15:30:18 -0500

    Added sections

--- code_examples/sections.cc
+++ code_examples/sections.cc
@@ -3,32 +3,50 @@
 
 using namespace std;
 
-void t1() {
+int t1(int x) {
 	printf("Task 1 is on thread %d \n", omp_get_thread_num());
+    int local_sum = 0;
+	for(int i = 0; i < x; i++){
+        local_sum += i;
+    }
+    return local_sum;
 }
-void t2() {
+int t2(int x) {
 	printf("Task 2 is on thread %d \n", omp_get_thread_num());
+    int local_sum = 0;
+	for(int i = 0; i < x; i++){
+        local_sum += i;
+    }
+    return local_sum;
 }
-void t3() {
-	printf("Task 3 is on thread %d \n", omp_get_thread_num());
+int t3(int x) {
+    printf("Task 3 is on thread %d \n", omp_get_thread_num());
+    int local_sum = 0;
+	for(int i = 0; i < x; i++){
+        local_sum += i;
+    }
+    return local_sum;
 }
 
 int main()
 {
-#pragma omp parallel sections 
-{
-#pragma omp section 
-{
-			t1();
-		}
-#pragma omp section 
-{
-			t2();
-		}
-#pragma omp section 
-{
-			t3();
-		}
-	}
-	return 0;
+    int t1_sum, t2_sum, t3_sum;
+#pragma omp parallel sections
+    {
+#pragma omp section
+        {
+            t1_sum = t1(100000);
+        }
+#pragma omp section
+        {
+            t2_sum = t2(100000);
+        }
+#pragma omp section
+        {
+            t3_sum = t3(100000);
+        }
+    }
+    int big_sum = t1_sum + t2_sum + t3_sum;
+    printf("100000*3 = %d \n", big_sum);
+    return 0;
 }
