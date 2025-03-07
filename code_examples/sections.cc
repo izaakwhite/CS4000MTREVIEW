cf26deac02de0aa0dea9f26e53f2c590ae742d1b
Izaak White <ww655421@ohio.edu>
2025-03-06 23:01:24 -0500

    yurr sending this shi up b4 mt gl ppl in the future

--- code_examples/sections.cc
+++ code_examples/sections.cc
@@ -9,22 +9,25 @@ int t1(int x) {
 	for(int i = 0; i < x; i++){
         local_sum += i;
     }
+    printf("Task 1 local sum: %d \n", local_sum);
     return local_sum;
 }
 int t2(int x) {
 	printf("Task 2 is on thread %d \n", omp_get_thread_num());
     int local_sum = 0;
 	for(int i = 0; i < x; i++){
-        local_sum += i;
+        local_sum += i * i;
     }
+    printf("Task 2 local sum: %d \n", local_sum);
     return local_sum;
 }
 int t3(int x) {
     printf("Task 3 is on thread %d \n", omp_get_thread_num());
     int local_sum = 0;
 	for(int i = 0; i < x; i++){
-        local_sum += i;
+        local_sum -= i;
     }
+    printf("Task 3 local sum: %d \n", local_sum);
     return local_sum;
 }
 
@@ -35,18 +38,18 @@ int main()
     {
 #pragma omp section
         {
-            t1_sum = t1(100000);
+            t1_sum = t1(100000000);
         }
 #pragma omp section
         {
-            t2_sum = t2(100000);
+            t2_sum = t2(400000);
         }
 #pragma omp section
         {
-            t3_sum = t3(100000);
+            t3_sum = t3(900000);
         }
     }
     int big_sum = t1_sum + t2_sum + t3_sum;
-    printf("100000*3 = %d \n", big_sum);
+    printf("big_sum = %d \n", big_sum);
     return 0;
 }
--- code_examples/semaphore.cc
+++ code_examples/semaphore.cc
@@ -2,6 +2,8 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
+#include <chrono>
 
 class Semaphore {
 private:
@@ -25,24 +27,38 @@ public:
     }
 };
 
-Semaphore sem(2);  // Allows up to 2 threads at a time
+Semaphore sem(4);  // Allows up to 4 threads at a time
+std::mutex log_mtx; // To prevent mixed output
+
+void log(const std::string& message, int id) {
+    std::lock_guard<std::mutex> guard(log_mtx);
+    auto now = std::chrono::steady_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+    std::cout << "[" << ms << " ms] Thread " << id << ": " << message << std::endl;
+}
 
 void task(int id) {
+    log("Waiting for semaphore...", id);
     sem.wait();
-    std::cout << "Thread " << id << " is inside the critical section\n";
+
+    log("Entered critical section", id);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "Thread " << id << " is leaving the critical section\n";
+
+    log("Leaving critical section", id);
     sem.signal();
 }
 
 int main() {
-    std::thread t1(task, 1);
-    std::thread t2(task, 2);
-    std::thread t3(task, 3);
+    const int num_threads = 16;
+    std::vector<std::thread> threads;
 
-    t1.join();
-    t2.join();
-    t3.join();
+    for (int i = 0; i < num_threads; ++i) {
+        threads.emplace_back(task, i + 1);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
 
     return 0;
 }
